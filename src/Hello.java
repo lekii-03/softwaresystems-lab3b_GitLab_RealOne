3145104902b8323f5c641b2c47d298d2317666d8
lekii-03 <95911976+lekii-03@users.noreply.github.com>
2021-12-10 17:52:06 +0100

    Revert "Exclamation mark added"
    This reverts commit 8fbcdf416330e5389c5cbed18c6c71b33e8fde16.

--- src/Hello.java
+++ src/Hello.java
@@ -1,14 +1,5 @@
 public class Hello {
-    public static void main(String[] args) {
-        HelloWorld();
-    }
-    public static void HelloWorld(){
-        System.out.println("Hello World");
-    }
 
-    public static void SpanishHello(){
-        System.out.println("Hola Amigo!");
-    }
-
+    
 }
 
